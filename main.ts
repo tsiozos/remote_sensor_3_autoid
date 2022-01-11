25c3f407c713f5b9e4593461cde346a24c762194
tsiozos <38419287+tsiozos@users.noreply.github.com>
2022-01-11 11:14:23 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+
+//device id is the serial number modulo 990 + 10
+//so range is actually 10 ~ 999
+let devID = Math.abs(control.deviceSerialNumber() % 990 + 10);
+
+serial.writeLine("my devID "+devID);
+basic.showString(devID.toString(),100);
+
+//Pressing A dumps data
+input.onButtonPressed(Button.A, function() {
+    serial.writeLine(encodeSensors());
+    basic.showString(devID.toString(), 100);
+})
+
+function encodeSensors() {
+    let transtrin=devID.toString()+";";
+    transtrin += "T:"+(input.temperature()-3.5)+";";
+    transtrin += "L:"+input.lightLevel()+";"; 
+    transtrin += "C:"+input.compassHeading()+";";
+    return transtrin;    
+}
+
+function transmitEverything() {
+    let transtr = encodeSensors();
+    serial.writeLine(transtr);
+}
+
+control.setInterval(function() {
+    transmitEverything()
+}, 1000, control.IntervalMode.Interval)
