9247b8ded9a7290affec3fd2a5805f204895429d
tsiozos <38419287+tsiozos@users.noreply.github.com>
2022-11-11 12:38:47 +0200

    Ενημέρωση main.ts

--- main.ts
+++ main.ts
@@ -16,15 +16,51 @@ input.onButtonPressed(Button.A, function() {
     basic.showString(devID.toString(), 100);
 })
 
+// Enums for sensor symbols
+class Sensors {
+    static STATION = "+"
+    static TIME = ">"
+    static TEMP = "@"
+    static LIGHT = "^"
+    static HEADING = "&"
+    static MAGNETIC = "%"
+    static HASH = "*"
+    static ALL = "+>@^&%*"
+};
+
+class SensorData {
+    public Station: string;
+    public Time: int32;
+    
+}
+
 function encodeSensors() {
-    let transtrin="+"+devID.toString();
-    transtrin += ">"+encode(Math.floor(input.runningTime() / 1000)%86400,62);
-    transtrin += "@"+encode((input.temperature()-4),62);
-    transtrin += "^"+encode(input.lightLevel(),62); 
-    //transtrin += "H"+encode(input.compassHeading(),62);
-    //transtrin += "M"+encode(Math.trunc(input.magneticForce(Dimension.Strength)),62);
-    transtrin += "*"+hash_string(transtrin);
+    let transtrin=Sensors.STATION+devID.toString();
+    transtrin += Sensors.TIME+encode(Math.floor(input.runningTime() / 1000)%86400,62);
+    transtrin += Sensors.TEMP+encode((input.temperature()-4),62);
+    transtrin += Sensors.LIGHT+encode(input.lightLevel(),62); 
+    //transtrin += Sensors.HEADING+encode(input.compassHeading(),62);
+    //transtrin += Sensors.MAGNETIC+encode(Math.trunc(input.magneticForce(Dimension.Strength)),62);
+    transtrin += Sensors.HASH+hash_string(transtrin);
     return transtrin;    
+}
+
+function extractValue(s: string, start: number): string {
+    let sval = "";
+    for (let i=start+1; i<s.length && (Sensors.ALL.indexOf(s[i]) == -1); i++)
+        sval += s[i];
+    return sval;
+}
+
+function decodeSensors(encoded: string, sens: Sensors): string {
+    let res = null;
+    if (Sensors.STATION === sens) {
+        if (encoded[0] == Sensors.STATION)
+            res = extractValue(encoded, 0);
+    } else if (Sensors.TIME === sens) {
+
+    }
+    return res;
 }
 
 /*
@@ -57,6 +93,11 @@ serial.writeLine("**** START ****")
 
 //for (let i=0; i< 300; i++)
 //    serial.writeLine(i+": "+encode(i,16));
-let s1 = encode(999999999,62);
-serial.writeLine("999999999 = 62x "+s1);
-serial.writeLine("62x "+s1+" = "+decode(s1,62).toString());
+//let s1 = encode(999999999,62);
+//serial.writeLine("999999999 = 62x "+s1);
+//serial.writeLine("62x "+s1+" = "+decode(s1,62).toString());
+
+//serial.writeLine("station="+extractValue("+45@44%53&180",0));
+//serial.writeLine("temp=" + extractValue("+45@44%53&180", 3));
+//serial.writeLine("heading=" + extractValue("+45@44%53&180", 9));
+
